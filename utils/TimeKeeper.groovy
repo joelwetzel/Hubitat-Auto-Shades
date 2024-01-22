4f55a72846b147a27a34011fd2d88cefeed1f00c
Joel Wetzel <jwetzel@affirma.com>
2024-01-22 16:50:10 -0600

    Document the TimeKeeper class

--- utils/TimeKeeper.groovy
+++ utils/TimeKeeper.groovy
@@ -2,6 +2,12 @@ package joelwetzel.auto_shades.utils
 
 import groovy.time.*
 
+/**
+* The TimeKeeper is a class that intercepts and overrides the default Date constructor.
+* This allows us to control the current time in tests, without having to build anything special into the app script itself.
+* As long as an app uses "new Date()" to get the current time, this class will control what time the app sees.
+* Note that you must call install() before the app script is run, and uninstall() after the app script is run.
+*/
 class TimeKeeper {
     private Date internalDate = null
 
@@ -13,8 +19,23 @@ class TimeKeeper {
         internalDate = startingDate
     }
 
+    /**
+    * Install the override of the default Date constructor
+    */
     def install() {
         Date.metaClass.constructor = { -> return this.now() }
+    }
+
+    /**
+    * Uninstall the override of the default Date constructor, so that "new Date()"
+    * will return the current time again.
+    */
+    def uninstall() {
+        Date.metaClass = null
+    }
+
+    def now() {
+        return internalDate
     }
 
     def set(Date newDate) {
@@ -35,13 +56,5 @@ class TimeKeeper {
 
     def advanceDays(int days) {
         internalDate = groovy.time.TimeCategory.plus(internalDate, new groovy.time.TimeDuration(days, 0, 0, 0, 0))
-    }
-
-    def now() {
-        return internalDate
-    }
-
-    def uninstall() {
-        Date.metaClass = null
     }
 }
